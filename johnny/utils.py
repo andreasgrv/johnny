32b39e30e9e6d5b4245e15a218827b91ed612a21
grv <andreasgrv@gmail.com>
2017-06-07 01:15:00 +0100

    Add BucketManager for faster batch train, shuffling

---
+++ johnny/utils.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+
+class BucketManager(object):
+
+    DATA_KEY = 'data'
+    INDEX_KEY = 'index'
+    END_INDEX_KEY = 'end_index'
+
+
+    def __init__(self, data, bucket_width, max_len, min_len=1, batch_size=64,
+                 shuffle=True, row_key=None):
+        """
+        data: a list of rows
+
+        bucket_width: int - how much of a difference in length is tolerable - 
+        hashed to the same bucket.
+
+        max_len: the maximum length of a row or row entry - includes the
+        maximum.
+
+        min_len: the minumum length of a row or row entry - default value of 1
+        means we start considering things of length 1 - if we have something
+        empty we will get an index error because we aren't considering 0
+        length objects.
+
+        shuffle: whether to shuffle entries in each bucket or not
+
+        row_key: callable - a function run on the row to compute a value to
+        use to map it to a bucket
+        """
+        super(BucketManager, self).__init__()
+        self.bucket_width = bucket_width
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        # by default we use the length of the first entry in the row to sort by
+        self.row_key = row_key or (lambda x: len(x))
+        self.batch_count = 0
+
+        assert(bucket_width > 0)
+        assert(max_len > 0)
+        assert(min_len >= 0)
+        self.max_len = max_len
+        self.min_len = min_len
+        bucket_range = self.max_len - self.min_len + 1
+        exact_fit = bucket_range // bucket_width 
+        has_remainder = int(bucket_range % bucket_width > 0)
+        self.num_buckets = exact_fit + has_remainder
+
+        self.buckets = [{self.DATA_KEY: [], self.END_INDEX_KEY: 0, self.INDEX_KEY: 0}
+                        for i in range(self.num_buckets)]
+
+        self.left_samples = np.zeros(self.num_buckets, dtype=np.float32)
+
+        for row in data:
+            length = self.row_key(row)
+            adjust_length = length - self.min_len + 1
+            remainder = int((adjust_length % self.bucket_width) > 0)
+            which_bucket = (adjust_length // self.bucket_width) + remainder - 1
+            if which_bucket < 0:
+                raise IndexError
+            target = self.buckets[which_bucket]
+            target[self.DATA_KEY].append(row)
+            target[self.END_INDEX_KEY] += 1
+            self.left_samples[which_bucket] += 1
+
+        if self.shuffle:
+            self.shuffle_bucket_contents()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        data = self.sample()
+        if data:
+            self.batch_count += 1
+            return data
+        else:
+            raise StopIteration
+
+    def shuffle_bucket_contents(self):
+        """Shuffles entries inside each bucket"""
+        for buck_indx in range(self.num_buckets):
+            target = self.buckets[buck_indx]
+            np.random.shuffle(target[self.DATA_KEY])
+
+    def reset(self, shuffle=None):
+        """Resets status"""
+        shuffle = shuffle or self.shuffle
+        if shuffle:
+            self.shuffle_bucket_contents()
+        for buck_indx in range(self.num_buckets):
+            target = self.buckets[buck_indx]
+            # rewinds index of each bucket
+            target[self.INDEX_KEY] = 0
+            self.left_samples[buck_indx] = target[self.END_INDEX_KEY]
+        self.batch_count = 0
+
+    def sample(self):
+        more_to_go = self.total_left
+        if more_to_go:
+            probs = self.left_samples/more_to_go
+            which_bucket = np.random.choice(self.num_buckets, 1, p=probs)[0]
+            #print('which_bucket',which_bucket)
+            bucket = self.buckets[which_bucket]
+            index = bucket[self.INDEX_KEY]
+            left_over = bucket[self.END_INDEX_KEY] - index
+            num_samples = min((self.batch_size, left_over))
+            data = bucket[self.DATA_KEY][index : index + num_samples]
+            bucket[self.INDEX_KEY] += num_samples
+            self.left_samples[which_bucket] -= num_samples
+            return data
+        return None
+
+    @property
+    def total_left(self):
+        return int(sum(self.left_samples))
+
+shades = [' ', '░', '▒', '▓', '█']
+
+# def shade(words, probs):
+#     return ' '.join([shades[int(prob/0.2)]*len(word)
+#                      for word, prob in zip(words, probs)])
+def shade(probs):
+    return ''.join([shades[int(prob/0.2)] for prob in probs])
---
+++ tests/test_util.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pytest
+from johnny.utils import BucketManager
+
+def test_basic():
+    data = [[1,2,3],
+            [4,5,6,7],
+            [8,9,3,2],
+            [4,5,6],
+            [3,4,5,6],
+            [1,2,3]]
+
+    batch_size = 3 
+    bm = BucketManager(data, 1, 4, batch_size=batch_size)
+    for each in bm:
+        assert len(each) == batch_size
+
+def test_uneven():
+    data = [[1,2,3],
+            [4,5,6],
+            [1,2,3]]
+
+    batch_size = 2 
+    bm = BucketManager(data, 1, 3, batch_size=batch_size)
+    batch = next(bm)
+    assert(len(batch) == 2)
+    batch = next(bm)
+    assert(len(batch) == 1)
+    with pytest.raises(StopIteration):
+        batch = next(bm)
+
+def test_wrong_max_len():
+    data = [[1,2,3],
+            [4,5,6],
+            [1,2,3]]
+
+    batch_size = 2 
+    with pytest.raises(IndexError):
+        bm = BucketManager(data, 1, 2, batch_size=batch_size)
+
+def test_bucket_width():
+    data = [[1,2,3],
+            [4,5,6],
+            [1,2,3],
+            [2,3],
+            [5,2],
+            ]
+
+    bucket_width = 3
+    bm = BucketManager(data, bucket_width, 3, batch_size=2)
+    assert(bm.buckets[0][bm.END_INDEX_KEY] == 5)
+    bucket_width = 2
+    bm = BucketManager(data, bucket_width, 3, batch_size=2)
+    assert(bm.buckets[0][bm.END_INDEX_KEY] == 2)
+    assert(bm.buckets[1][bm.END_INDEX_KEY] == 3)
+
+def test_nonzero_start():
+    data = [[1,2,3],
+            [4,5,6],
+            [1,2,3],
+            [2,3],
+            [5,2],
+            ]
+
+    bucket_width = 2
+    bm = BucketManager(data, bucket_width, 3, batch_size=2)
+    assert(bm.buckets[0][bm.END_INDEX_KEY] == 2)
+    assert(bm.buckets[1][bm.END_INDEX_KEY] == 3)
+
+    bucket_width = 2
+    bm = BucketManager(data, bucket_width, 4, min_len=2, batch_size=2)
+    assert(bm.buckets[0][bm.END_INDEX_KEY] == 5)
+
+def test_zero_length():
+    data = [[],
+            [],
+            [],
+            [2,3],
+            [5,2],
+            ]
+
+    bucket_width = 2
+    with pytest.raises(IndexError):
+        bm = BucketManager(data, bucket_width, 2, batch_size=2)
+    bm = BucketManager(data, bucket_width, 2, min_len=0, batch_size=2)
+    assert(bm.buckets[0][bm.END_INDEX_KEY] == 3)
+    assert(bm.buckets[1][bm.END_INDEX_KEY] == 2)
+    bucket_width = 3
+    bm = BucketManager(data, bucket_width, 2, min_len=0, batch_size=2)
+    assert(bm.buckets[0][bm.END_INDEX_KEY] == 5)
+
+def test_uneven_unshuffled():
+    data = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
+
+    batch_size = 2 
+    bm = BucketManager(data, 1, 3, batch_size=batch_size, shuffle=False)
+    batch = next(bm)
+    assert(batch == [[1,2,3],[4,5,6]])
+    batch = next(bm)
+    assert(batch == [[7,8,9]])
+    with pytest.raises(StopIteration):
+        batch = next(bm)
+
+def test_reset():
+    data = [[1,2,3],
+            [4,5,6]]
+
+    batch_size = 2 
+    bm = BucketManager(data, 1, 3, batch_size=batch_size, shuffle=False)
+    batch = next(bm)
+    assert(batch == [[1,2,3],[4,5,6]])
+    with pytest.raises(StopIteration):
+        batch = next(bm)
+    bm.reset()
+    batch = next(bm)
+    assert(batch == [[1,2,3],[4,5,6]])
+    with pytest.raises(StopIteration):
+        batch = next(bm)
+
+
+def test_row_key():
+    data = [[[1,2,3],[1,2,3]],
+            [[1,2],[1,2]],
+            [[3,4,5],[3,4,5]],
+            [[3,4],[3,4]],
+            [[6,7,8],[6,7,8]]
+            ]
+
+    batch_size = 2 
+    bm = BucketManager(data, 1, 3, batch_size=batch_size, shuffle=False, row_key=lambda x: len(x[0]))
+    assert(bm.buckets[0][bm.DATA_KEY] == [])
+    assert(bm.buckets[1][bm.END_INDEX_KEY] == 2)
+    assert(bm.buckets[2][bm.END_INDEX_KEY] == 3)
+    batch = next(bm)
+    assert(len(batch) == 2)
+    batch = next(bm)
+    batch = next(bm)
+    with pytest.raises(StopIteration):
+        batch = next(bm)
+    bm = BucketManager(data, 1, 3, batch_size=batch_size, shuffle=False, row_key=lambda x: len(x))
+    assert(bm.buckets[0][bm.DATA_KEY] == [])
+    assert(bm.buckets[1][bm.END_INDEX_KEY] == 5)
+    assert(bm.buckets[2][bm.END_INDEX_KEY] == 0)
