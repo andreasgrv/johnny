d37c439da4fbeeef3c2b27e13df344b9e1bf7643
grv <andreasgrv@gmail.com>
2017-07-02 22:11:31 +0100

    Add out_size and size params + return tuple

--- tests/test_udep_structs.py
+++ tests/test_udep_structs.py
@@ -5,4 +5,4 @@ from collections import namedtuple
 def test_arclen():
     t = namedtuple('TokenStub', ('head'))
     s = Sentence([t(3), t(1), t(0), t(2)])
-    assert(s.arc_lengths == [2, 1, 1, 2])
+    assert(s.arc_lengths == (2, 1, 1, 2))
--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -5,22 +5,22 @@ def test_from_token_list():
     v = Vocab.from_token_list(s.split(), size=7)
     assert(len(v) == len(set(s.split())))
     e = v.encode('unknown words'.split())
-    assert(e == [0, 0])
+    assert(e == (0, 0))
     e = v.encode('the daybreak supercalifragilistic'.split())
-    assert(e[0] != v.UNK)
-    assert(e[1] != v.UNK)
-    assert(e[2] == v.UNK)
+    assert(e[0] != v.reserved.UNK)
+    assert(e[1] != v.reserved.UNK)
+    assert(e[2] == v.reserved.UNK)
 
 def test_zero_size():
     s = 'daybreak at the bottom of the lake' # note there are 2 "the"
     v = Vocab.from_token_list(s.split(), size=0)
     assert(len(v) == 0)
     e = v.encode('unknown words'.split())
-    assert(e == [0, 0])
+    assert(e == (0, 0))
     e = v.encode('the daybreak supercalifragilistic'.split())
-    assert(e[0] == v.UNK)
-    assert(e[1] == v.UNK)
-    assert(e[2] == v.UNK)
+    assert(e[0] == v.reserved.UNK)
+    assert(e[1] == v.reserved.UNK)
+    assert(e[2] == v.reserved.UNK)
 
 def test_threshold():
     s = 'daybreak at the bottom of the lake' # note there are 2 "the"
@@ -28,11 +28,11 @@ def test_threshold():
     print(v.index)
     assert(len(v) == 1)
     e = v.encode('unknown words'.split())
-    assert(e == [0, 0])
+    assert(e == (0, 0))
     e = v.encode('the daybreak supercalifragilistic'.split())
-    assert(e[0] != v.UNK)
-    assert(e[1] == v.UNK)
-    assert(e[2] == v.UNK)
+    assert(e[0] != v.reserved.UNK)
+    assert(e[1] == v.reserved.UNK)
+    assert(e[2] == v.reserved.UNK)
 
 def test_serialisation(tmpdir):
     f = tmpdir.join('v.vocab')
